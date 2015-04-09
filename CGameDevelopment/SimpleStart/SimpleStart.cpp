31299afc3a625974f42016825f8f230dbce9b980
djangojazz <brettmorin2@gmail.com>
2015-04-09 11:52:59 -0700

    added code to simple program

--- CGameDevelopment/SimpleStart/SimpleStart.cpp
+++ CGameDevelopment/SimpleStart/SimpleStart.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "What is your name?" << endl;
+
+	string name{};
+	cin >> name;
+
+	cout << "You said your name is: " << name << endl;
+
 	return 0;
 }
 
