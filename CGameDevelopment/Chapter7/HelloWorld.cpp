068cc97903ffc1d5484e177436b15ac095399ca4
djangojazz <brettmorin2@gmail.com>
2015-05-11 15:40:44 -0700

    revert

--- CGameDevelopment/Chapter7/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include <iostream>
-
-using namespace std;
-
-void HelloWorld()
-{
-	cout << "Hello World!" << endl;
-}
--- CGameDevelopment/TextAdventureGame/PlayerOptions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum class PlayerOptions
-{
-	Quit,
-	None
-};
