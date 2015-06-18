0dbfaad39ba763317ab8a125461719f3b09e34f7
djangojazz <brettmorin2@gmail.com>
2015-06-18 13:09:03 -0700

    Made some changes to Chapter 22

--- CGameDevelopment/Chapter22/Chapter22.cpp
+++ CGameDevelopment/Chapter22/Chapter22.cpp
@@ -2,6 +2,10 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <memory>
+
+using namespace std;
 
 void function2(int variable1)
 {
@@ -13,10 +17,39 @@ void function1(int variable)
 	function2(variable);
 }
 
+class Simple
+{
+private:
+	int variable{ 0 };
+
+public:
+	Simple()
+	{
+		cout << "Constructed" << endl;
+	}
+	~Simple()
+	{
+		cout << "Destroyed" << endl;
+	}
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int variable{ 0 };
-	function1(variable);
+	using UniqueSimplePtr = unique_ptr<Simple>;
+	UniqueSimplePtr pSimple1{ new Simple() };
+	cout << pSimple1.get() << endl;
+
+	UniqueSimplePtr pSimple2;
+	pSimple2.swap(pSimple1);
+	cout << pSimple1.get() << endl;
+	cout << pSimple2.get() << endl;
+
+	using IntSharedPtr = shared_ptr<int>;
+	IntSharedPtr pIntArray1{ new int[16] };
+	IntSharedPtr pIntArray2{ pIntArray1 };
+
+	cout << endl << pIntArray1.get() << endl;
+	cout << pIntArray2.get() << endl;
 
 	return 0;
 }
